83d0e179db870a12c40ee731c45bd3859c3d0489
ramyashrict <ramyarr1415@gmail.com>
2023-04-19 14:15:11 +0530

    Added program

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Game
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder game!");
+            Console.WriteLine("Press Enter key to roll the dice...");
+            Atposition0_UC1.call();
+        }
+    }
+}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to snakeladder");
+        }
+    }
+}
